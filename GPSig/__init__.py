806ed95958f4f0e20ecc3ac94f6b44b147661329
Csaba <t.g.csaba@gmail.com>
2019-06-19 11:06:59 +0200

    Remove redundant files

--- GPSig/__init__.py
+++ GPSig/__init__.py
@@ -5,5 +5,3 @@ from . import models
 from . import optimizers
 from . import preprocessing
 from . import training
-
-# __all__ = ['feature_maps', 'helpers', 'kernels', 'low_rank_calculations', 'models', 'optimizers', 'path_filters', 'rand', 'training']
