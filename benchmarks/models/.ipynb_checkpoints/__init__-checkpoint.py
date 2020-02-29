dabdea61ca766d2870cf180c40ba4d8ab2dfa139
Csaba <t.g.csaba@gmail.com>
2020-02-29 16:34:35 +0100

    remove ipynb checkpoint folders

--- benchmarks/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .train_gpkconv1d import train_gpkconv1d_classifier
-from .train_gprnn import train_gprnn_classifier
-from .train_gpsig import train_gpsig_classifier
-from .train_gpsigrnn import train_gpsigrnn_classifier
--- gpsig/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from . import kernels
-from . import models
-from . import inducing_variables
-from . import preprocessing
-from . import training
-from . import utils
